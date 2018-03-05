78c47ab98db41f98d1e4ca75e380f8760389fdfc
laincarl <1047972853@qq.com>
2018-03-05 20:51:29 +0800

    ssh测试

--- src/component/manage/ManageMenu.js
+++ src/component/manage/ManageMenu.js
@@ -4,6 +4,7 @@
  * @Last Modified by:   LainCarl 
  * @Last Modified time: 2018-03-05 20:33:56 
  */
+
 import React, { Component } from 'react';
 import { Menu } from 'antd';
 import { inject } from 'mobx-react';
