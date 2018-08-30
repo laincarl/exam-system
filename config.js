cff5a2d21366ae1ef7bac8eb5de386d2b396334a
laincarl <1047972853@qq.com>
2018-08-30 12:01:53 +0800

    [ADD]使用https

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const version = process.env.NODE_ENV;
 const build_time = process.env.BUILD_TIME;
 window.console.log(`%c${version}:${build_time}`, 'color:#1496DD');
 const config = {
-  server: version === 'development' ? 'http://localhost:9000/' : 'http://123.207.142.127:9000/',
+  server: version === 'development' ? 'https://localhost:9000/' : 'http://123.207.142.127:9000/',
 };
 export default config;
